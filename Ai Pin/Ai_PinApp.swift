19ef8d32e2b3da2be0270a8ac7c70d2f0faba50f
Eric Lewis <ericlewis777@gmail.com>
2024-04-23 16:40:19 -0400

    Initial Commit

---
+++ Ai Pin/Ai_PinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ai_PinApp.swift
+//  Ai Pin
+//
+//  Created by Eric Lewis on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Ai_PinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ai Pin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ai Pin
+//
+//  Created by Eric Lewis on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
