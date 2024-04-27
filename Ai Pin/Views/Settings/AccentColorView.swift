8ebbe3f900cfb4a0c59bfeb44365eaba16569722
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-04-27 12:51:12 -0600

    Added Appearance settings sheets

--- Ai Pin/Views/Settings/AccentColorView.swift
+++ Ai Pin/Views/Settings/AccentColorView.swift
@@ -8,13 +8,80 @@
 import SwiftUI
 
 struct AccentColorView: View {
+    @EnvironmentObject private var colorStore: ColorStore
+    
+    @State private var accentColor = Color.blue
+    @State private var originalColor = Color.blue
+    @State private var triggerSave = false
+    
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        VStack {
-            Text("Contrary to popular belief, Lorem Ipsum is not simply random text.")
-                .foregroundStyle(.blue)
-            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries")
-                .foregroundStyle(.white)
+        VStack(alignment: .leading) {
+            Text("This is an example title")
+                .foregroundStyle(accentColor)
+                .padding([.bottom], 5)
+                .bold()
+                .font(Font.system(size: 20))
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem lacus, scelerisque ut ultrices in, aliquam ut massa. Vestibulum sed iaculis diam. In pulvinar felis nibh, ultrices bibendum tellus elementum ut. Ut non turpis feugiat, molestie justo sed.")
+                .foregroundStyle(.foreground)
         }
+        .padding()
+        .background(.thinMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+        .padding(13)
+        .padding([.top], 20)
+        .onAppear() {
+            let tempColor = colorStore.loadColor()
+            accentColor = tempColor
+            originalColor = tempColor
+        }
+        
+        Spacer()
+        
+        ColorPicker("", selection: $accentColor, supportsOpacity: true)
+            .labelsHidden()
+            .foregroundStyle(.foreground)
+            .padding()
+            .scaleEffect(CGSize(width: 2.5, height: 2.5))
+            .padding([.bottom])
+        
+        HStack(spacing: 8) {
+            Button {
+                triggerSave.toggle()
+                colorStore.setColor(color: accentColor)
+                dismiss()
+            } label: {
+                HStack(spacing: 5) {
+                    Text("Save")
+                        .bold()
+                    Image(systemName: "checkmark")
+                }
+            }
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 15.0))
+            .sensoryFeedback(.selection, trigger: triggerSave)
+            
+            if (originalColor != accentColor) {
+                Button {
+                    colorStore.setColor(color: originalColor)
+                    accentColor = originalColor
+                } label: {
+                    HStack(spacing: 5) {
+                        Text("Reset")
+                            .bold()
+                        Image(systemName: "arrow.counterclockwise")
+                    }
+                }
+                .foregroundStyle(.black)
+                .padding()
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 15.0))
+            }
+        }
+        .padding([.bottom], 20)
     }
 }
 
---
+++ Ai Pin/Views/Settings/IconChangerView.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+struct IconItem: Identifiable {
+    var id: UUID
+    var title: String
+    var iconName: String
+    var imageName: String
+}
+
+struct IconChangerView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    var icons = [
+        IconItem(id: UUID(), title: "Classic", iconName: "", imageName: "AppIconPreview"),
+        IconItem(id: UUID(), title: "Stargaze", iconName: "StarIcon", imageName: "StarIconPreview"),
+        IconItem(id: UUID(), title: "Ai Pin", iconName: "DeviceIcon", imageName: "DeviceIconPreview"),
+        IconItem(id: UUID(), title: ".Center", iconName: "LogoIconDark", imageName: "LogoIconDarkPreview"),
+        IconItem(id: UUID(), title: "Sensors", iconName: "SensorsIcon", imageName: "SensorsIconPreview"),
+        IconItem(id: UUID(), title: "Ai Pin Dark", iconName: "TextLogoDark", imageName: "TextLogoDarkPreview"),
+        IconItem(id: UUID(), title: "Ai Pin Light", iconName: "TextLogoLight", imageName: "TextLogoLightPreview")]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 40) {
+                ForEach(icons, id: \.self.id) { icon in
+                    Button {
+                        changeAppIcon(to: icon.iconName)
+                    } label: {
+                        VStack(alignment: .center, spacing: 8) {
+                            Image(icon.imageName)
+                                .resizable()
+                                .frame(width: 130, height: 130)
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 20.0))
+                            Text(icon.title)
+                                .font(Font.system(.title2))
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+            }
+            .padding([.leading, .trailing])
+        }
+        .scrollIndicators(.hidden)
+    }
+    
+    
+    private func changeAppIcon(to iconName: String) {
+        UIApplication.shared.setAlternateIconName(iconName != "" ? iconName : nil) { error in
+            if let error = error {
+                print("Error setting alternate icon \(error.localizedDescription)")
+            }
+
+        }
+        
+        dismiss()
+    }
+}
+
+#Preview {
+    IconChangerView()
+}
