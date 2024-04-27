7b38f0fdfe0a679e6c0b96a4281f5156aa01c36d
Eric Lewis <ericlewis777@gmail.com>
2024-04-26 23:42:13 -0400

    copy & save (photo) captures

--- Ai Pin/Views/CapturesView.swift
+++ Ai Pin/Views/CapturesView.swift
@@ -28,6 +28,19 @@ struct CapturesView: View {
                                 .aspectRatio(1.333, contentMode: .fit)
                         }
                         .listRowInsets(.init())
+                        .contextMenu {
+                            // TODO: video handling
+                            Button("Copy Photo", systemImage: "doc.on.doc") {
+                                Task {
+                                    try await UIPasteboard.general.image = image(for: capture)
+                                }
+                            }
+                            Button("Save Photo", systemImage: "square.and.arrow.down") {
+                                Task {
+                                    try await save(capture: capture)
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -54,13 +67,29 @@ struct CapturesView: View {
     
     func load() async {
         do {
-            let captures = try await API.shared.captures()
+            let captures = try await API.shared.captures(size: 100)
             withAnimation {
                 state.captures = captures.content
             }
         } catch {
             print(error)
         }
+    }
+    
+    func save(capture: Capture) async throws {
+        try await UIImageWriteToSavedPhotosAlbum(image(for: capture), nil, nil, nil)
+    }
+    
+    func image(for capture: Capture) async throws -> UIImage {
+        let (data, _) = try await URLSession.shared.data(from: URL(string: "https://webapi.prod.humane.cloud/capture/memory/\(capture.uuid)/file/\(capture.data.thumbnail!.fileUUID)")!.appending(queryItems: [
+            .init(name: "token", value: capture.data.thumbnail!.accessToken),
+            .init(name: "w", value: "640"),
+            .init(name: "q", value: "100")
+        ]))
+        guard let image = UIImage(data: data) else {
+            fatalError()
+        }
+        return image
     }
 }
 
