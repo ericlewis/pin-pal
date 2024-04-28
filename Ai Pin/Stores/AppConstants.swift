17dce44e7dba5e949acd3b26a2bf5672296e118e
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-04-27 20:45:20 -0600

    fix: Added App Constants

---
+++ Ai Pin/Stores/AppConstants.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+import Foundation
+
+struct Constants {
+    static let UI_CUSTOM_ACCENT_COLOR_V1 = "accentColor"
+}
--- Ai Pin/Stores/ColorStore.swift
+++ Ai Pin/Stores/ColorStore.swift
@@ -4,19 +4,19 @@ import SwiftUI
     var accentColor: Color = .blue;
     
     init() {
-        if let _ = UserDefaults.standard.object(forKey: "accentColor") {
+        if let _ = UserDefaults.standard.object(forKey: Constants.UI_CUSTOM_ACCENT_COLOR_V1) {
             self.accentColor = loadColor()
         }
     }
     
     func setColor(color: Color) {
         let cgColor = UIColor(color).cgColor
-        UserDefaults.standard.set(cgColor.components, forKey: "accentColor")
+        UserDefaults.standard.set(cgColor.components, forKey: Constants.UI_CUSTOM_ACCENT_COLOR_V1)
         accentColor = color
     }
     
     func loadColor() -> Color {
-        guard let colorArray = UserDefaults.standard.object(forKey: "accentColor") as? [CGFloat] else {
+        guard let colorArray = UserDefaults.standard.object(forKey: Constants.UI_CUSTOM_ACCENT_COLOR_V1) as? [CGFloat] else {
             return Color.blue
         }
         
