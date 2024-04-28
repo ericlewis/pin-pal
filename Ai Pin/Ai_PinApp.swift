bf250d9fc64610dc1792948daff96db38c89d65f
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-04-27 20:28:43 -0600

    Fixed observable and added app tint color

--- Ai Pin/Ai_PinApp.swift
+++ Ai Pin/Ai_PinApp.swift
@@ -4,21 +4,29 @@ import AppIntents
 @main
 struct Ai_PinApp: App {
     
-    @State private var sceneNavigationStore: NavigationStore
-    @StateObject private var colorStore = ColorStore()
+    @State 
+    private var sceneNavigationStore: NavigationStore
+    
+    @State
+    private var sceneColorStore: ColorStore
     
     init() {
         let navigationStore = NavigationStore()
         sceneNavigationStore = navigationStore
+        
+        let colorStore = ColorStore()
+        sceneColorStore = colorStore
 
         AppDependencyManager.shared.add(dependency: navigationStore)
+        AppDependencyManager.shared.add(dependency: colorStore)
     }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(sceneNavigationStore)
-                .environmentObject(colorStore)
+                .environment(sceneColorStore)
+                .tint(sceneColorStore.accentColor)
         }
     }
 }
