c33336e91ab94c87194ab0fc35611c0324d98075
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-04-30 19:11:27 -0600

    fix: only decode trackId if present

--- PinKit/Sources/PinKit/Model/PageableContentEnvelope.swift
+++ PinKit/Sources/PinKit/Model/PageableContentEnvelope.swift
@@ -52,7 +52,7 @@ struct MusicEvent: Codable {
         self.albumArtUuid = try container.decodeIfPresent(UUID.self, forKey: .albumArtUuid)
         self.length = try container.decodeIfPresent(String.self, forKey: .length)
         self.sourceService = try container.decode(String.self, forKey: .sourceService)
-        self.trackID = try container.decode(String.self, forKey: .trackID)
+        self.trackID = try container.decodeIfPresent(String.self, forKey: .trackID)
         guard let playlistData = try container.decodeIfPresent(String.self, forKey: .generatedPlaylist)?.data(using: .utf8) else {
             self.generatedPlaylist = nil
             return
