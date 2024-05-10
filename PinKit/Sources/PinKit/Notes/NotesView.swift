11e482b74d2b8689865149bd439ee3973e9972cf
Eric Lewis <ericlewis777@gmail.com>
2024-05-10 10:52:38 -0400

    arrange notes

--- PinKit/Sources/PinKit/Notes/NotesView.swift
+++ PinKit/Sources/PinKit/Notes/NotesView.swift
@@ -25,21 +25,8 @@ struct NotesView: View {
     @State
     private var query = ""
 
-    var predicate: Predicate<Note> {
-        if app.noteFilter.type == .all {
-            return #Predicate<Note> { _ in
-                true
-            }
-        } else {
-            return #Predicate<Note> {
-                $0.isFavorite
-            }
-        }
-    }
-
     var body: some View {
         @Bindable var navigation = navigation
-        @Bindable var noteFilter = app.noteFilter
         var filter = app.noteFilter.filter
         let _ = filter.sortBy = [app.noteFilter.sort]
         let _ = filter.predicate = predicate
@@ -52,47 +39,13 @@ struct NotesView: View {
             .refreshable(action: load)
             .searchable(text: $query)
             .overlay(alignment: .bottom) {
-                SyncStatusView(current: \.numberOfNotesSynced, total: \.totalNotesToSync)
+                SyncStatusView(
+                    current: \.numberOfNotesSynced,
+                    total: \.totalNotesToSync
+                )
             }
             .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Menu("New Note", systemImage: "plus") {
-                        Button("Create", systemImage: "note.text.badge.plus", intent: OpenNewNoteIntent())
-                        Button("Import", systemImage: "square.and.arrow.down", intent: OpenFileImportIntent())
-                    } primaryAction: {
-                        self.navigation.activeNote = .create()
-                    }
-                }
-                ToolbarItemGroup(placement: .secondaryAction) {
-                    Menu("Filter", systemImage: "line.3.horizontal.decrease.circle") {
-                        Toggle(isOn: noteFilter.type == .all, intent: FilterNotesIntent(filter: .all)) {
-                            Label("All Items", systemImage: "note.text")
-                        }
-                        Section {
-                            Toggle(isOn: noteFilter.type == .favorites, intent: FilterNotesIntent(filter: .favorites)) {
-                                Label("Favorites", systemImage: "heart")
-                            }
-                        }
-                    }
-                    .symbolVariant(noteFilter.type == .all ? .none : .fill)
-                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
-                        SortNotesToggle("Name", sortBy: \.name)
-                        SortNotesToggle("Body", sortBy: \.body)
-                        SortNotesToggle("Created At", sortBy: \.createdAt)
-                        SortNotesToggle("Modified At", sortBy: \.modifiedAt)
-                        Section("Order") {
-                            Picker("Order", selection: $noteFilter.order) {
-                                Label("Ascending", systemImage: "arrow.up").tag(SortOrder.forward)
-                                Label("Descending", systemImage: "arrow.down").tag(SortOrder.reverse)
-                            }
-                            .onChange(of: noteFilter.order) {
-                                withAnimation(.snappy) {
-                                    noteFilter.sort.order = noteFilter.order
-                                }
-                            }
-                        }
-                    }
-                }
+                toolbar
             }
             .navigationTitle("Notes")
         }
@@ -101,26 +54,70 @@ struct NotesView: View {
         }
         .fileImporter(
             isPresented: $navigation.fileImporterPresented,
-            allowedContentTypes: [.plainText]
-        ) { result in
-            Task.detached {
-                do {
-                    switch result {
-                    case let .success(success):
-                        let str = try String(contentsOf: success)
-                        self.navigation.activeNote = .init(text: str, title: success.lastPathComponent)
-                    case let .failure(failure):
-                        break
-                    }
-                } catch {
-                    print(error)
-                }
-            }
-        }
+            allowedContentTypes: [.plainText],
+            onCompletion: handleImport
+        )
         .task(initial)
         .task(id: query, search)
     }
     
+    var predicate: Predicate<Note> {
+        if app.noteFilter.type == .all {
+            return #Predicate<Note> { _ in
+                true
+            }
+        } else {
+            return #Predicate<Note> {
+                $0.isFavorite
+            }
+        }
+    }
+    
+    @ToolbarContentBuilder
+    var toolbar: some ToolbarContent {
+        @Bindable var noteFilter = app.noteFilter
+        ToolbarItem(placement: .primaryAction) {
+            Menu("New Note", systemImage: "plus") {
+                Button("New Note", systemImage: "note.text.badge.plus", intent: OpenNewNoteIntent())
+                Button("Import Text", systemImage: "square.and.arrow.down", intent: OpenFileImportIntent())
+            } primaryAction: {
+                self.navigation.activeNote = .create()
+            }
+        }
+        ToolbarItemGroup(placement: .secondaryAction) {
+            Menu("Filter", systemImage: "line.3.horizontal.decrease.circle") {
+                Toggle(isOn: noteFilter.type == .all, intent: FilterNotesIntent(filter: .all)) {
+                    Label("All Items", systemImage: "note.text")
+                }
+                Section {
+                    Toggle(isOn: noteFilter.type == .favorites, intent: FilterNotesIntent(filter: .favorites)) {
+                        Label("Favorites", systemImage: "heart")
+                    }
+                }
+            }
+            .symbolVariant(noteFilter.type == .all ? .none : .fill)
+            Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                SortNotesToggle("Name", sortBy: \.name)
+                SortNotesToggle("Body", sortBy: \.body)
+                SortNotesToggle("Created At", sortBy: \.createdAt)
+                SortNotesToggle("Modified At", sortBy: \.modifiedAt)
+                Section("Order") {
+                    Picker("Order", selection: $noteFilter.order) {
+                        Label("Ascending", systemImage: "arrow.up").tag(SortOrder.forward)
+                        Label("Descending", systemImage: "arrow.down").tag(SortOrder.reverse)
+                    }
+                    .onChange(of: noteFilter.order) {
+                        withAnimation(.snappy) {
+                            noteFilter.sort.order = noteFilter.order
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension NotesView {
     func search() async {
         do {
             try await Task.sleep(for: .milliseconds(300))
@@ -165,34 +162,20 @@ struct NotesView: View {
         isLoading = false
         isFirstLoad = false
     }
-}
-
-struct SortNotesToggle: View {
     
-    let name: LocalizedStringKey
-    let sortBy: KeyPath<Note, String>?
-    let sortBy2: KeyPath<Note, Date>?
-
-    init(_ name: LocalizedStringKey, sortBy: KeyPath<Note, String>) {
-        self.name = name
-        self.sortBy = sortBy
-        self.sortBy2 = nil
-    }
-    
-    init(_ name: LocalizedStringKey, sortBy: KeyPath<Note, Date>) {
-        self.name = name
-        self.sortBy = nil
-        self.sortBy2 = sortBy
-    }
-    
-    @Environment(AppState.self)
-    private var app
-    
-    var body: some View {
-        if let sortBy2 {
-            Toggle(name, isOn: app.noteFilter.sort.keyPath == sortBy, intent: SortNotesIntent(sortBy: sortBy2))
-        } else if let sortBy {
-            Toggle(name, isOn: app.noteFilter.sort.keyPath == sortBy, intent: SortNotesIntent(sortBy: sortBy))
+    func handleImport(_ result: Result<URL, any Error>) {
+        Task.detached {
+            do {
+                switch result {
+                case let .success(success):
+                    let str = try String(contentsOf: success)
+                    self.navigation.activeNote = .init(text: str, title: success.lastPathComponent)
+                case let .failure(failure):
+                    break
+                }
+            } catch {
+                print(error)
+            }
         }
     }
 }
---
+++ PinKit/Sources/PinKit/Notes/SortNotesToggle.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct SortNotesToggle: View {
+    
+    let name: LocalizedStringKey
+    let sortBy: KeyPath<Note, String>?
+    let sortBy2: KeyPath<Note, Date>?
+
+    init(_ name: LocalizedStringKey, sortBy: KeyPath<Note, String>) {
+        self.name = name
+        self.sortBy = sortBy
+        self.sortBy2 = nil
+    }
+    
+    init(_ name: LocalizedStringKey, sortBy: KeyPath<Note, Date>) {
+        self.name = name
+        self.sortBy = nil
+        self.sortBy2 = sortBy
+    }
+    
+    @Environment(AppState.self)
+    private var app
+    
+    var body: some View {
+        if let sortBy2 {
+            Toggle(name, isOn: app.noteFilter.sort.keyPath == sortBy, intent: SortNotesIntent(sortBy: sortBy2))
+        } else if let sortBy {
+            Toggle(name, isOn: app.noteFilter.sort.keyPath == sortBy, intent: SortNotesIntent(sortBy: sortBy))
+        }
+    }
+}
+
